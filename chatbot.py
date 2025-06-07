b61178fab4730030c2be479619b6be489e10bbce
Kim-Hee-Choong <midalobba@gmail.com>
2025-06-07 13:44:44 +0900

    최종 결과 커밋

--- chatbot.py
+++ chatbot.py
@@ -1,35 +1,72 @@
 import pandas as pd
 
-# sklearn라는 머신러닝 라이브러리에서 TfidfVectorizer와 cosine_similarity를 불러옴
-# TfidfVectorizer는 문서의 텍스트 데이터를 벡터 형태로 변환하는데 사용하며, cosine_similarity는 두 벡터 간의 코사인 유사도를 계산
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
 # 챗봇 클래스를 정의
 class SimpleChatBot:
-    # 챗봇 객체를 초기화하는 메서드, 초기화 시에는 입력된 데이터 파일을 로드하고, TfidfVectorizer를 사용해 질문 데이터를 벡터화함
+    # 챗봇 객체를 초기화하는 메서드, 초기화 시에는 입력된 데이터 파일을 로드하고, 질문과 답변을 모두 리스트로 만듬
     def __init__(self, filepath):
         self.questions, self.answers = self.load_data(filepath)
-        self.vectorizer = TfidfVectorizer()
-        self.question_vectors = self.vectorizer.fit_transform(self.questions)
-
+        
+        
     # CSV 파일로부터 질문과 답변 데이터를 불러오는 메서드
     def load_data(self, filepath):
         data = pd.read_csv(filepath)
         questions = data['Q'].tolist()
-        questions = data['A'].tolist()
+        answers = data['A'].tolist()
         return questions, answers
 
-    # 입력 문장에 가장 잘 맞는 답변을 찾는 메서드, 입력 문장을 벡터화하고, 이를 기존 질문 벡터들과 비교하여 가장 높은 유사도를 가진 질문의 답변을 반환함
+    # 입력 문장에 가장 잘 맞는 답변을 찾는 메서드, 챗봇에 입력된 문장과 학습시 사용된 질문과 가장 유사한 답변을 레벤슈타인 거리 알고리즘을 이용해서 구한다.
     def find_best_answer(self, input_sentence):
-        # 사용자 입력 문장을 벡터화
-        input_vector = self.vectorizer.transform([input_sentence])
-        # 사용자 입력 벡터와 기존 질문 벡터들 간의 코사인 유사도를 계산
-        similarities = cosine_similarity(input_vector, self.question_vectors)
-        # 가장 유사도가 높은 질문의 인덱스를 찾음
-        best_match_index = similarities.argmax()
+        
+        # 사용자 입력과 학습 데이터의 질문 전체와의 레벤슈타인 거리를 루프를 돌면서 계산하고, 이중 가장 작은 인덱스를 구한다.
+        min_index = 0
+        min_value = 999999
+        for i in range(0, len(self.questions)):
+            if self.calc_distance(input_sentence, self.questions[i]) < min_value :
+                min_index = i
+                min_value = self.calc_distance(input_sentence, self.questions[i])
+    
         # 가장 유사한 질문에 해당하는 답변을 반환
-        return self.answers[best_match_index]
+        return self.answers[min_index]
+    
+    # 레벤슈타인 거리 구하기
+    def calc_distance(self, a, b):
+        ''' 레벤슈타인 거리 계산하기 '''
+        if a == b: return 0 # 같으면 0을 반환
+        a_len = len(a) # a 길이
+        b_len = len(b) # b 길이
+        if a == "": return b_len
+        if b == "": return a_len
+        # 2차원 표 (a_len+1, b_len+1) 준비하기 --- (※1)
+        # matrix 초기화의 예 : [[0, 1, 2, 3], [1, 0, 0, 0, 0], [2, 0, 0, 0, 0], [3, 0, 0, 0, 0], [4, 0, 0, 0, 0]]
+        # [0, 1, 2, 3]
+        # [1, 0, 0, 0]
+        # [2, 0, 0, 0]
+        # [3, 0, 0, 0] 
+        matrix = [[] for i in range(a_len+1)] # 리스트 컴프리헨션을 사용하여 1차원 초기화
+        for i in range(a_len+1): # 0으로 초기화
+            matrix[i] = [0 for j in range(b_len+1)]  # 리스트 컴프리헨션을 사용하여 2차원 초기화
+        # 0일 때 초깃값을 설정
+        for i in range(a_len+1):
+            matrix[i][0] = i
+        for j in range(b_len+1):
+            matrix[0][j] = j
+        # 표 채우기 --- (※2)
+        # print(matrix,'----------')
+        for i in range(1, a_len+1):
+            ac = a[i-1]
+            # print(ac,'=============')
+            for j in range(1, b_len+1):
+                bc = b[j-1] 
+                # print(bc)
+                cost = 0 if (ac == bc) else 1  #  파이썬 조건 표현식 예:) result = value1 if condition else value2
+                matrix[i][j] = min([
+                    matrix[i-1][j] + 1,     # 문자 제거: 위쪽에서 +1
+                    matrix[i][j-1] + 1,     # 문자 삽입: 왼쪽 수에서 +1   
+                    matrix[i-1][j-1] + cost # 문자 변경: 대각선에서 +1, 문자가 동일하면 대각선 숫자 복사
+                ])
+                # print(matrix)
+            # print(matrix,'----------끝')
+        return matrix[a_len][b_len]
 
 # 데이터 파일의 경로를 지정합니다.
 filepath = 'ChatbotData.csv'
